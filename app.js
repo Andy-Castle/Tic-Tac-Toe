0bec7ed6f3319885376800b6d9a33bc722f23388
Andy-Castle <masterchief_and@live.com>
2024-09-02 21:05:42 -0500

    .

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const winningArrays = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+  [0, 4, 8], [2, 4, 6]             // Diagonals
+];
+
+
 const Game = (function () {
   let table = ["", "", "", "", "", "", "", "", ""];
   const makeTable = () => table;
